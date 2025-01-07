434246e1d559b56c35aa418edbd6ddb84d1ecb00
joschonarth <joschonarth@gmail.com>
2025-01-07 19:28:58 -0300

    feat: implement user guess verification and reveal correct letters

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import { Button } from "./components/Button"
 import { LettersUsed, LettersUsedProps } from "./components/LettersUsed"
 
 export default function App() {
-  const [attempts, setAttempts] = useState(0)
+  const [score, setScore] = useState(0)
   const [letter, setLetter] = useState("")
   const [lettersUsed, setLettersUsed] = useState<LettersUsedProps[]>([])
   const [challenge, setChallenge] = useState<Challenge | null>(null)
@@ -26,7 +26,39 @@ export default function App() {
     
     setChallenge(randomWord)
     
-    setAttempts(0)
+    setScore(0)
+    setLetter("")
+    setLettersUsed([])
+  }
+
+  function handleConfirm() {
+    if (!challenge) {
+      return
+    }
+
+    if (!letter.trim()) {
+      return alert("Digite uma letra!")
+    }
+
+    const value = letter.toUpperCase()
+    const exists = lettersUsed.find(
+      (used) => used.value.toLocaleUpperCase() === value
+    )
+
+    if (exists) {
+      return alert("Você já utilizou a letra " + value)
+    }
+
+    const hits = challenge.word
+      .toUpperCase()
+      .split("")
+      .filter((char) => char === value).length
+
+    const correct = hits > 0
+    const currentScore = score + hits
+
+    setLettersUsed((prevState) => [...prevState, { value, correct }])
+    setScore(currentScore)
     setLetter("")
   }
 
@@ -41,23 +73,40 @@ export default function App() {
   return (
     <div className={styles.container}>
       <main>
-        <Header current={attempts} max={10} onRestart={handleRestartGame}/>
+        <Header current={score} max={10} onRestart={handleRestartGame}/>
         
-        <Tip tip="Uma das linguagens de programação mais utilizadas" />
+        <Tip tip={challenge.tip} />
         
         <div className={styles.word}>
           {
-            challenge.word.split("").map(() => (
-              <Letter value="" />
-            ))
+            challenge.word.split("").map((letter, index) => {
+              const letterUsed = lettersUsed.find(
+                (used) => used.value.toUpperCase() === letter.toUpperCase()
+              )
+              
+              return (
+                <Letter 
+                  key={index} 
+                  value={letterUsed?.value} 
+                  color={letterUsed?.correct ? "correct" : "default"} 
+                />
+              )
+            })
           }      
         </div>
 
         <h4>Palpite</h4>
 
         <div className={styles.guess}>
-          <Input autoFocus maxLength={1} placeholder="?" />
-          <Button title="Confirmar" />
+          <Input 
+            autoFocus 
+            maxLength={1} 
+            placeholder="?"
+            value={letter}
+            onChange={(e) => setLetter(e.target.value)} 
+          />
+
+          <Button title="Confirmar" onClick={handleConfirm} />
         </div>
 
         <LettersUsed data={lettersUsed} />
