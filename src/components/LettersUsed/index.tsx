ba7d0cd2fcb9f8ab64a52f63f6fb0472d17d3f04
joschonarth <joschonarth@gmail.com>
2025-01-07 18:50:31 -0300

    feat: add functionality to list used letters

--- src/components/LettersUsed/index.tsx
+++ src/components/LettersUsed/index.tsx
@@ -2,18 +2,30 @@ import styles from "./styles.module.css"
 
 import { Letter } from "../Letter"
 
-export function LettersUsed() {
+export type LettersUsedProps = {
+    value: string
+    correct: boolean
+}
+
+type Props = {
+    data: LettersUsedProps[]
+}
+
+export function LettersUsed({ data }: Props) {
     return (
         <div className={styles.lettersUsed}>
             <h5>Letras utilizadas</h5>
             
             <div>
-                <Letter value="R" size="small" color="correct" />
-                <Letter value="L" size="small" color="wrong" />
-                <Letter value="M" size="small" color="wrong" />
-                <Letter value="P" size="small" color="wrong" />
-                <Letter value="U" size="small" color="wrong" />
-                <Letter value="X" size="small" color="wrong" />
+                {
+                    data.map(({ value, correct }) => (
+                        <Letter 
+                            value={value} 
+                            size="small" 
+                            color={correct ? "correct" : "wrong"} 
+                        />
+                    ))
+                }
             </div>
         </div>
     )
