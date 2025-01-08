c960e3a3330c4dd12507a9ffefb91cbd66aafa44
joschonarth <joschonarth@gmail.com>
2025-01-08 15:23:15 -0300

    feat: add more words and tips to the challenge list

--- src/utils/words.ts
+++ src/utils/words.ts
@@ -8,10 +8,11 @@ export const WORDS: Challenge[] = [
     { id: 1, word: "CSS", tip: "Linguagem de estilos" },
     { id: 2, word: "REACT", tip: "Biblioteca para criar interfaces Web" },
     { id: 3, word: "HTML", tip: "Linguagem de marcação" },
-    {
-        id: 4,
-        word: "Javascript",
-        tip: "Uma das linguagens de programação mais utilizadas no mundo",
-    },
-    { id: 5, word: "Typescript", tip: "Para adicionar tipagem no Javascript"}
+    { id: 4, word: "Javascript", tip: "Uma das linguagens de programação mais utilizadas no mundo" },
+    { id: 5, word: "Typescript", tip: "Para adicionar tipagem no Javascript" },
+    { id: 6, word: "PYTHON", tip: "Linguagem conhecida por sua simplicidade e versatilidade" },
+    { id: 7, word: "ANGULAR", tip: "Framework para criar aplicações web robustas" },
+    { id: 8, word: "GIT", tip: "Sistema de controle de versão amplamente utilizado" },
+    { id: 9, word: "NODE", tip: "Ambiente para execução de JavaScript no lado do servidor" },
+    { id: 10, word: "JAVA", tip: "Linguagem orientada a objetos amplamente utilizada em aplicações corporativas" }
 ]
